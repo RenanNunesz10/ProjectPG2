7355271f0b3c238bb9d75178d538f7a378f0db68
Renan Nunes <142848799+RenanNunesz10@users.noreply.github.com>
2024-05-23 22:06:53 +0000

    Changes

--- src/main/java/com/example/ProjetoPG2/entidade/Cidade.java
+++ src/main/java/com/example/ProjetoPG2/entidade/Cidade.java
@@ -6,7 +6,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="cidade")
+@Table(name="cidades")
 public class Cidade {
     @Id @GeneratedValue
     private Long id;
